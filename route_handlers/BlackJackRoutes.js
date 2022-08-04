acee45e6a73b59e8b5cfe4d8637e585079e0552d
Joshua Frazer <joshua.frazer.school@gmail.com>
2022-08-04 09:08:12 -0700

    blackjack route handling

--- route_handlers/BlackJackRoutes.js
+++ route_handlers/BlackJackRoutes.js
@@ -1,29 +1,51 @@
 const router = require('express').Router();
 const UserModel = require('../models/User');
-const TransactionModel = require('../models/Transaction')
+const TransactionModel = require('../models/Transaction');
+const Payments = require('../gameLogic/Payment');
+const Blackjack = require('../gameLogic/BlackJack');
 
 class BlackJackRoutes {
+    static BlackjackGame = {};
 
     static async getBlackJack(request, response){
         
+
     }
 
-    static postBlackJack(request, response){
-        
+    static async postBlackJack(request, response){
+        console.log(request.body);
+        BlackJackRoutes.BlackjackGame[request.user.email] = new Blackjack();
+        await BlackJackRoutes.BlackjackGame[request.user.email].fetchDeck(6);
+        let winStatus = await BlackJackRoutes.BlackjackGame[request.user.email].initialDeal();
+        let playersCards = BlackJackRoutes.BlackjackGame[request.user.email].getPlayersCards();
+        let dealersCards = BlackJackRoutes.BlackjackGame[request.user.email].getDealersCards();
+        let playerScore = BlackJackRoutes.BlackjackGame[request.user.email].scoreHand(playersCards);
+        let dealerScore = BlackJackRoutes.BlackjackGame[request.user.email].scoreHand(dealersCards);
+        console.log(`bet: ${request.body.bet}`);
+        BlackJackRoutes.BlackjackGame[request.user.email].bet = request.body.bet;
+        console.log(BlackJackRoutes.BlackjackGame[request.user.email]);
+        response.send({
+            winStatus: winStatus, 
+            playersCards: playersCards,
+            dealersCards: dealersCards,
+            playerScore: playerScore,
+            dealerScore: dealerScore
+        });
+        //TO DO: if won, record transaction and end game 
     }
 
-    static deleteBlackJack(request, response){
-      
+    static async deleteBlackJack(request, response){
+        BlackJackRoutes.BlackjackGame[request.user.email] = null;
     }
 
-    static updateBlackJack(request, response){
+    static async updateBlackJack(request, response){
        
     }
 }
 
 router.get('/', BlackJackRoutes.getBlackJack);
 router.post('/', BlackJackRoutes.postBlackJack);
-router.delete('/:id', BlackJackRoutes.deleteBlackJack);
-router.put('/:id', BlackJackRoutes.updateBlackJack);
+router.delete('/', BlackJackRoutes.deleteBlackJack);
+router.put('/', BlackJackRoutes.updateBlackJack);
 
 module.exports = router;
